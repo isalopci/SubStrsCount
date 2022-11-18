14ebdabc319034211dfc169037682a460e49895f
Isabella <isalopci@gmail.com>
2022-11-18 04:01:46 -0500

    Changed implementation to class and methods

--- test2/repeatedSubstrings.py
+++ test2/repeatedSubstrings.py
@@ -1,56 +1,58 @@
 import re
 
-# Python function that identifies the substrings of 4 characters that appear 
+# Python class that identifies the substrings of 4 characters that appear 
 #        along a main string of random length and content.
-
-def generateSubStrings(originalStr: str) -> list:
-    #Variable to store a list of substrings
-    listSubStr = []
+class repeatedSubstrings:
     
-    # First loop which indicates the first index of the current substring
-    for i in range(len(originalStr)-3):
-
-        subStr = ""
+    def __init__(self) -> None:
+        pass
+    
+    def generateSubStrings(self, originalStr: str) -> list:
+        #Variable to store a list of substrings
+        listSubStr = []
         
-        # Second loop to generate a 4 char substring
-        for j in range(i,i+4):
-            subStr += originalStr[j]
+        # First loop which indicates the first index of the current substring
+        for i in range(len(originalStr)-3):
 
-        # add substring to the list of substrings
-        listSubStr.append(subStr)
-    
-    return listSubStr
+            subStr = ""
+            
+            # Second loop to generate a 4 char substring
+            for j in range(i,i+4):
+                subStr += originalStr[j]
 
-# Python function that identifies how many substrings of 4 characters appear more
-#        than once along a main string of random length and content.
+            # add substring to the list of substrings
+            listSubStr.append(subStr)
+        
+        return listSubStr
 
-def countSubStrs(originalStr: str) -> list[dict]:
 
-    #Create an empty dictionary to store each substring with more than once instance
-    subStrDict = {}
-    
-    #Obtains list of substrings in the original string 
-    listSubStr = generateSubStrings(originalStr)
+    def countSubStrs(self, originalStr: str) -> list[dict]:
 
-    # For each substring or 'word' in the list 
-    for word in listSubStr:
+        #Create an empty dictionary to store each substring with more than once instance
+        subStrDict = {}
+        
+        #Obtains list of substrings in the original string 
+        listSubStr = self.generateSubStrings(originalStr)
 
-        # Regular expression to get the indexes on the original string where there is an instance of the word
-        listIndexes = [i.start() for i in re.finditer(word, originalStr)]
+        # For each substring or 'word' in the list 
+        for word in listSubStr:
 
-        #Number of instances
-        count = len(listIndexes)
+            # Regular expression to get the indexes on the original string where there is an instance of the word
+            listIndexes = [i.start() for i in re.finditer(word, originalStr)]
 
-        #Adds a substring (in the dictionary) that has more than one instance in the original string
-        if count > 1:
-            subStrDict[word] = count
+            #Number of instances
+            count = len(listIndexes)
 
-    return subStrDict
+            #Adds a substring (in the dictionary) that has more than one instance in the original string
+            if count > 1:
+                subStrDict[word] = count
+
+        return subStrDict
 
 
 # Driver Code to test 
 if __name__ == '__main__':
 
     originalStr = "ilovebooksoncemybooksalwaymysbooksforevermybooks"
-    subStrDict = countSubStrs(originalStr)
+    subStrDict = repeatedSubstrings().countSubStrs(originalStr)
     print(subStrDict) 
